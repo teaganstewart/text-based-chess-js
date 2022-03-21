385327268ae8633ba54f5a18e95109e26eeda9f3
Teagan Stewart <65583615+teaganstewart@users.noreply.github.com>
2022-03-21 20:17:17 +1300

    Tested Stylised Printing
    Added stylised printing and worked out the logic before implementing it completely. This is the next step.

--- logic.js
+++ logic.js
@@ -9,20 +9,25 @@
 
 class Board {
 
-    board = [[]];
+    boardArray = [[]];
     
+    /**
+     * 
+     * @param {*} boardFile 
+     */
     fileParser(boardFile) {
         let rows = boardFile.split(/\n/);
 
         // Reads only the piece data of the board - not the design parts (|'s and -'s)
-        for(let i = 1; i < 6; i++) {            
-            let cols = rows[(i*2)-1].split("     |     ")
+        for(let i = 0; i < 8; i++) {            
+            let cols = rows[i].split("     |     ")
             cols[0] = cols[0].split("|     ")[1];
             
             // Add pieces to the board
-            this.board[i] = []
+            this.boardArray[i] = []
             for(let j = 0; j < 8; j++) {
-                this.board[i].push(cols[j]);
+                //console.log(cols[j]);
+                this.boardArray[i].push(cols[j]);
             }
         }     
     }
@@ -59,15 +64,6 @@ class Board {
 //================ PIECE CLASS ======================
 //===================================================
 
-// class Piece {
-
-// }
-
-
-const PADDING = "    ";
-
-//TODO: method for printing the board + creating and storing the board.
-
 /**
  * Board pieces are represented by letters. These are:
  * 
@@ -80,17 +76,61 @@ const PADDING = "    ";
  * King - K
  * 
  */
+class Piece {
+
+    whiteStyle = 'font-weight: bold; font-size: 20px; color: white;)';
+    orangeStyle = 'font-weight: bold; font-size: 20px; color: orange;)';
+
+    constructor(value, color) {
+        this.value = value;
+        this.color = color;
+    }
+
+    get getColor() {
+        return this.color;
+    }
+
+}
 
 
+//===================================================
+//============== BOARD FUNCTIONS ====================
+//===================================================
 
 function createBoard() {   
     board.loadBoard();
-      
+        
 }
 
 function parseBoard(boardFile) {
-    console.log(boardFile)
     board.fileParser(boardFile);
+    printBoard();
+}
+
+
+
+function printBoard() {
+    // let whiteStyle = 'font-weight: bold; font-size: 20px;color: white;';
+    // let blackStyle = 'font-weight: bold; font-size: 20px;color: orange;';
+    // let normalStyle = 'font-weight: normal, font-size 12px; color: lightgrey;'
+
+    // let string = [whiteStyle, blackStyle, whiteStyle, blackStyle, whiteStyle, blackStyle, whiteStyle, whiteStyle]
+    // console.log("%c%s %c%s %c%s %c%s %c%s %c%s %c%s %c%s", string[0],"R", string[1], "R",string[2], "R",string[3], "R",
+    // string[4], "R",string[5], "R",string[6], "R",string[7], "R");
+    for(var i = 0; i < 8; i++) {
+        var str = "|\t\t";
+        for(var j = 0; j < 8; j++) {
+            if(i!=0) {
+                str += board.boardArray[i][j] + " \t\t|\t\t";
+            }
+            else {
+                str += board.boardArray[i][j] + " \t\t|\t\t";
+            }
+            
+        }
+        console.log(str);
+        console.log("-----------------------------------------------------------------------------")
+    }
 }
 
 
@@ -99,6 +139,7 @@ function parseBoard(boardFile) {
 let board = new Board();
 
 createBoard();
+
 
 // console.log("----------------------------------------------------------");
 
